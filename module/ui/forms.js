443ded7f9ac962de1c050c76fe5478b5fd746929
Anthony Blackshaw <ant@getme.co.uk>
2018-07-22 19:03:45 +0100

    Added character count support

--- module/ui/forms.js
+++ module/ui/forms.js
@@ -1,4 +1,5 @@
 import * as $ from 'manhattan-essentials'
+import {CharacterCount} from 'manhattan-character-count'
 import {datePicker} from 'manhattan-date-picker'
 import {addFilled} from 'manhattan-field-filled'
 
@@ -14,32 +15,39 @@ export function init() {
         = '.mh-field__text, .mh-field__textarea, .mh-field__select'
 
     for (inputElm of $.many(inputsSelector)) {
-        $.listen(
-            inputElm,
-            {
-                'blur': (event) => {
-                    $.closest(event.target, '.mh-field__control')
-                        .classList.remove('mh-field__control--focused')
-                },
-                'empty': (event) => {
-                    $.closest(event.target, '.mh-field__control')
-                        .classList.remove('mh-field__control--filled')
-                },
-                'filled': (event) => {
-                    $.closest(event.target, '.mh-field__control')
-                        .classList.add('mh-field__control--filled')
-                },
-                'focus': (event) => {
-                    $.closest(event.target, '.mh-field__control')
-                        .classList.add('mh-field__control--focused')
+
+        if ($.closest(inputElm, '.mh-field__control')) {
+            $.listen(
+                inputElm,
+                {
+                    'blur': (event) => {
+                        $.closest(event.target, '.mh-field__control')
+                            .classList.remove('mh-field__control--focused')
+                    },
+                    'empty': (event) => {
+                        $.closest(event.target, '.mh-field__control')
+                            .classList.remove('mh-field__control--filled')
+                    },
+                    'filled': (event) => {
+                        $.closest(event.target, '.mh-field__control')
+                            .classList.add('mh-field__control--filled')
+                    },
+                    'focus': (event) => {
+                        $.closest(event.target, '.mh-field__control')
+                            .classList.add('mh-field__control--focused')
+                    }
                 }
-            }
-        )
+            )
+        }
     }
 
     addFilled(inputsSelector)
 
-    // @@ Character counters
+    // Character counters
+    for(inputElm of $.many('[data-mh-character-count]')) {
+        let characterCount = new CharacterCount(inputElm)
+        characterCount.init()
+    }
 
     // Date pickers
     for (inputElm of $.many('[data-mh-date-picker]')) {
