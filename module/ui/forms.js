9909d143e96430bf8b3220fb4c0df0a27bbd58db
Anthony Blackshaw <ant@getme.co.uk>
2018-07-22 18:35:23 +0100

    Added support for squeezing field labels

--- module/ui/forms.js
+++ module/ui/forms.js
@@ -1,17 +1,46 @@
 import * as $ from 'manhattan-essentials'
+import {addFilled} from 'manhattan-field-filled'
 
 
 // -- Initializer --
 
 export function init() {
 
-    console.log('Initialize forms')
+    // Squeeze labels
+    const inputsSelector
+        = '.mh-field__text, .mh-field__textarea, .mh-field__select'
 
-    // @@ Squeeze labels
+    for (let inputElm of $.many(inputsSelector)) {
+        $.listen(
+            inputElm,
+            {
+                'blur': (event) => {
+                    $.closest(event.target, '.mh-field__control')
+                        .classList.remove('mh-field__control--focused')
+                },
+                'empty': (event) => {
+                    $.closest(event.target, '.mh-field__control')
+                        .classList.remove('mh-field__control--filled')
+                },
+                'filled': (event) => {
+                    $.closest(event.target, '.mh-field__control')
+                        .classList.add('mh-field__control--filled')
+                },
+                'focus': (event) => {
+                    $.closest(event.target, '.mh-field__control')
+                        .classList.add('mh-field__control--focused')
+                }
+            }
+        )
+    }
+
+    addFilled(inputsSelector)
 
     // @@ Character counters
 
-    // @@ Date pickers
+    // Date pickers
+
+    // @@ Map fields???
 
     // @@ Typeaheads
 
--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,13 @@ module.exports = {
     },
 
     externals: {
+        'manhattan-character-count': 'manhattan-character-count',
+        'manhattan-date-picker': 'manhattan-date-picker',
+        'manhattan-effects': 'manhattan-effects',
         'manhattan-essentials': 'manhattan-essentials',
-        'manhattan-sortable': 'manhattan-sortable'
+        'manhattan-field-filled': 'manhattan-field-filled',
+        'manhattan-sortable': 'manhattan-sortable',
+        'manhattan-typeahead': 'manhattan-typeahead'
     },
 
     output: {
