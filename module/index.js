adc0f743cb9326e9927381a4a35651f9f511dca4
Anthony Blackshaw <ant@getme.co.uk>
2018-07-22 13:45:56 +0100

    Base file added for forms

--- module/index.js
+++ module/index.js
@@ -1,4 +1,5 @@
 import * as changeLog from './ui/change-log'
+import * as forms from './ui/forms'
 import * as navigation from './ui/navigation'
 import * as ordering from './ui/ordering'
 import * as results from './ui/results'
@@ -10,6 +11,7 @@ export function init() {
 
     // UI
     changeLog.init()
+    forms.init()
     navigation.init()
     ordering.init()
     results.init()
---
+++ module/ui/forms.js
@@ -0,0 +1,24 @@
+import * as $ from 'manhattan-essentials'
+
+
+// -- Initializer --
+
+export function init() {
+
+    console.log('Initialize forms')
+
+    // @@ Squeeze labels
+
+    // @@ Character counters
+
+    // @@ Date pickers
+
+    // @@ Typeaheads
+
+    // @@ Tokenizers
+
+    // @@ Assets fields
+
+    // @@ Galleries
+
+}
